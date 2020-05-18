ecaad92860780e05d89908c2095d7f481d323595
satyasaibhushan <satyasaibhushan@gmail.com>
2020-05-19 02:15:26 +0530

    Done positioning of tasks according original order

--- src/js/tasksComponents/functionalities/apiManagement.js
+++ src/js/tasksComponents/functionalities/apiManagement.js
@@ -25,45 +25,50 @@ export default {
             taskDivs: [],
             checkedDivs: [],
           };
-          api.listTasks(ele.id).then((task) => {
-            console.log(task);
-            task.forEach((element, i) => {
-              if (element.status == "needsAction" && !element.parent)
-                taskListElement.taskDivs.push(
-                    this.newTask(
-                        false,
-                        element.title,
-                        -1,
-                        element.id
-                      ));
-              else if (element.parent && element.status != "completed") {
-                {
-                  let parentIndex = taskListElement.taskDivs
-                    .map((ele) => ele.id)
-                    .indexOf(element.parent);
-                  if (parentIndex != -1)
-                    taskListElement.taskDivs[parentIndex].children.push(
-                      this.newTask(
-                        false,
-                        element.title,
-                        parentIndex,
-                        element.id,
-                        element.parent
-                      )
-                    );
+          api
+            .listTasks(ele.id)
+            .then((task) => {
+                task.sort(function(a, b){return a.position - b.position});
+              console.log(task);
+              task.forEach((element, i) => {
+                if (element.status == "needsAction" && !element.parent) {
+                  taskListElement.taskDivs.push(
+                    this.newTask(false, element.title, -1, element.id)
+                  );
                 }
-              } else if (element.status == "completed")
-                taskListElement.checkedDivs.push(
-                  this.newTask(
-                    true,
-                    element.title,
-                    -1,
-                    element.id,
-                    element.parent ? element.parent : ""
-                  )
-                );
+              });
+              return task;
+            })
+            .then((task) => {
+              task.forEach((element, i) => {
+                if (element.parent && element.status != "completed") {
+                  {
+                    let parentIndex = taskListElement.taskDivs
+                      .map((ele) => ele.id)
+                      .indexOf(element.parent);
+                      console.log(element.position);
+                      taskListElement.taskDivs[parentIndex].children.push(
+                        this.newTask(
+                          false,
+                          element.title,
+                          parentIndex,
+                          element.id,
+                          element.parent
+                        )
+                      );
+                  }
+                } else if (element.status == "completed")
+                  taskListElement.checkedDivs.push(
+                    this.newTask(
+                      true,
+                      element.title,
+                      -1,
+                      element.id,
+                      element.parent ? element.parent : ""
+                    )
+                  );
+              });
             });
-          });
           taskList.push(taskListElement);
           taskComponent.setState({ taskList });
           // console.log(taskList);
